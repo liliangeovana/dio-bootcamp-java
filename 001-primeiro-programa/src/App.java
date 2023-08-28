856920a914ea54bce11a1a55dacdbd31f8c8790b
Lílian Saraiva <lili.ggsb@gmail.com>
2023-08-28 15:36:25 -0400

    primeiro programa

---
+++ 001-primeiro-programa/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá, mundo!");
+        System.out.println("Começando o novo aprendizado com o pé direito!");
+    }
+}
