c998a02626421efd51f741cc6e128d745e65bdf6
Lílian Saraiva <lili.ggsb@gmail.com>
2023-09-06 16:43:45 -0400

    uso do operador ternário

---
+++ 008-controle-fluxo/src/OperadorTernario.java
@@ -0,0 +1,8 @@
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int nota = 7;
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota <= 7 ? "Recuperação!" :"Reprovado";
+        System.out.println(resultado);
+    }
+}
