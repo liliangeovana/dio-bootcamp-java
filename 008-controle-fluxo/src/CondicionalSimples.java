2aadfa4713694f1f89c1029048537bfc761c4ddf
Lílian Saraiva <lili.ggsb@gmail.com>
2023-09-06 16:08:48 -0400

    uso de condicional simples

---
+++ 008-controle-fluxo/src/CondicionalSimples.java
@@ -0,0 +1,13 @@
+//EXEMPLO CAIXA ELETRÔNICO - CONDICIONAL SIMPLES
+public class CondicionalSimples {
+    public static void main (String[] args){
+       double saldo =25.0;
+       double valorSolicitado = 17.0;
+       
+       if (valorSolicitado < saldo){
+        saldo = saldo - valorSolicitado;
+       }
+
+       System.out.println(saldo);
+    }
+}
