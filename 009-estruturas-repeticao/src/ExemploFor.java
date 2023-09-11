bf8183b457293c3669fc6399327bd4ad14d346d4
Lílian Saraiva <lili.ggsb@gmail.com>
2023-09-11 15:53:09 -0400

    estrutura for

---
+++ 009-estruturas-repeticao/src/ExemploFor.java
@@ -0,0 +1,8 @@
+public class ExemploFor{
+    public static void main(String[] args) {
+        System.out.print("Contando carneirinhos: ");
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
+            System.out.print(carneirinhos + " ");
+        }
+    }
+}
