6b92c5aea2817604f764c32bf56dd2f62f30bfb5
Lílian Saraiva <lili.ggsb@gmail.com>
2023-09-11 16:06:32 -0400

    estrutura for com array

---
+++ 009-estruturas-repeticao/src/ForArray.java
@@ -0,0 +1,9 @@
+public class ForArray {
+    public static void main(String[] args) {
+        String consoantes[]={"A", "E", "I", "O", "U"};
+
+        for (int cont=0; cont <=consoantes.length; cont++){
+            System.out.println("A consoante de índice "+ cont + " é "+consoantes[cont]);
+        }
+    }
+}
