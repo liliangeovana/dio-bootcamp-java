b7e076f07426235722aca3a1112daf5ec6a5ddb9
Lílian Saraiva <lili.ggsb@gmail.com>
2023-09-11 16:19:37 -0400

    estrutura for abreviada

---
+++ 009-estruturas-repeticao/src/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String consoanteS[]={"A", "E", "I", "O", "U"};
+        
+        for (String consoante : consoanteS){
+            System.out.println("A consoante é "+consoante);
+        }
+    }
+}
