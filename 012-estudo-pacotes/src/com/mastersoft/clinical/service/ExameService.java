dc377d9ee434ef728266eb21a32f9258428e72c0
Lílian Saraiva <lili.ggsb@gmail.com>
2023-09-22 17:30:34 -0400

    criação e funcionamento de pacotes

---
+++ 012-estudo-pacotes/src/com/mastersoft/clinical/service/ExameService.java
@@ -0,0 +1,10 @@
+//Essa classe realiza as operações do exame como marcar, consultar o exame.
+package com.mastersoft.clinical.service;
+
+import com.mastersoft.clinical.model.Exame;
+
+public class ExameService {
+    void salvarExame(Exame exame){
+
+    }
+}
